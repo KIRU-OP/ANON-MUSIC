bbd1f3559f29ae67bb20fa11af322d503813d0e6
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <np564605@gmail.com>
2026-08-18 05:46:06 +0530

    Update import paths from VENOMMUSIC to AnonMusic

--- AnonMusic/platforms/Youtube.py
+++ AnonMusic/platforms/Youtube.py
@@ -15,17 +15,17 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from py_yt import VideosSearch
 
-from VENOMMUSIC.utils.cookie_handler import COOKIE_PATH
-from VENOMMUSIC.utils.database import is_on_off
-from VENOMMUSIC.utils.downloader import download_audio_concurrent, yt_dlp_download
-from VENOMMUSIC.utils.errors import capture_internal_err
-from VENOMMUSIC.utils.formatters import time_to_seconds
-from VENOMMUSIC.utils.tuning import (
+from AnonMusic.utils.cookie_handler import COOKIE_PATH
+from AnonMusic.utils.database import is_on_off
+from AnonMusic.utils.downloader import download_audio_concurrent, yt_dlp_download
+from AnonMusic.utils.errors import capture_internal_err
+from AnonMusic.utils.formatters import time_to_seconds
+from AnonMusic.utils.tuning import (
     YTDLP_TIMEOUT,
     YOUTUBE_META_MAX,
     YOUTUBE_META_TTL,
 )
-from VENOMMUSIC import LOGGER
+from AnonMusic import LOGGER
 
 _module_logger = LOGGER(__name__)
 
